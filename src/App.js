511105a933d635ae815bb3b81a7b2bbdc3760f9e
DmitriyKlimenko1995 <klimati1000@gmail.com>
2025-07-02 20:37:40 +0300

    first shoot

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Nav from './components/Navbar';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Nav />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
